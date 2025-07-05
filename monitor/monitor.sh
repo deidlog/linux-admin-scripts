31c39d561ce19129fe05673b32ed102585173e1e
Ryan <Gosling>
2025-07-05 12:54:00 +0300

    fix: add script exit on error and fix syntax error

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 LOG_DIR="/var/log/monitor"
 CPU_THRESHOLD=80
@@ -18,8 +19,8 @@ log_metric() {
 }
 
 # CPU usage
-CPU_USAGE=$(top -nb1 | grep "Cpu(s)") | awk '{print 100 - $8}'
-CPU_USAGE_INT=$(CPU_USAGE%.*)
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+CPU_USAGE_INT=${CPU_USAGE%.*}
 
 # RAM usage
 MEM_TOTAL=$(free -m | awk '/^Mem:/ {print $2}')
@@ -31,20 +32,20 @@ DISK_USAGE=$(df / | tail -1 | awk '{print $5}' | tr -d '%')
 
 # Network usage (rx/tx)
 NET_INTERFACE="$NETWORK_INTERFACE"
-RX_BYTES_BEFORE=$(cat /sys/class/net/$NETWORK_INTERFACE/statistics/rx_bytes)
-TX_BYTES_BEFORE=$(cat /sys/class/net/$NETWORK_INTERFACE/statistics/tx_bytes)
+RX_BYTES_BEFORE=$(cat /sys/class/net/$NET_INTERFACE/statistics/rx_bytes)
+TX_BYTES_BEFORE=$(cat /sys/class/net/$NET_INTERFACE/statistics/tx_bytes)
 sleep 1
-RX_BYTES_AFTER=$(cat /sys/class/net/$NETWORK_INTERFACE/statistics/rx_bytes)
-TX_BYTES_AFTER=$(cat /sys/class/net/$NETWORK_INTERFACE/statistics/tx_bytes)
+RX_BYTES_AFTER=$(cat /sys/class/net/$NET_INTERFACE/statistics/rx_bytes)
+TX_BYTES_AFTER=$(cat /sys/class/net/$NET_INTERFACE/statistics/tx_bytes)
 RX_RATE=$(( (RX_BYTES_AFTER - RX_BYTES_BEFORE) / 1024 )) # KB/s
 TX_RATE=$(( (TX_BYTES_AFTER - TX_BYTES_BEFORE) / 1024 )) # KB/s
 
 # Log data
-log_metric "CPU: $CPU_USAGE% | RAM: $MEM_USAGE% | Disk: $DISK_USAGE% | RX: ${RX_RATE}KB/s | TX: $(TX_RATE)KB/s"
+log_metric "CPU: $CPU_USAGE% | RAM: $MEM_USAGE% | Disk: $DISK_USAGE% | RX: ${RX_RATE}KB/s | TX: ${TX_RATE}KB/s"
 
 # Threshold alerts
 [[ "$CPU_USAGE_INT" -gt "$CPU_THRESHOLD" ]] && log_metric "HIGH CPU USAGE: $CPU_USAGE%"
-[[ "$MEM_USAGE" -gt "$MEM_THRESHOLD" ]] && log_metic "HIGHT MEMORY USAGE: $MEM_USAGE%"
+[[ "$MEM_USAGE" -gt "$MEM_THRESHOLD" ]] && log_metric "HIGHT MEMORY USAGE: $MEM_USAGE%"
 [[ "$DISK_USAGE" -gt "$DISK_THRESHOLD" ]] && log_metric "HIGH DISK USAGE: $DISK_USAGE%"
 
 exit 0
